38a3e0664f0b2ae42db5e2c3a4615a8178ff0f56
Code Kill <steve.rives@gmail.com>
2024-08-25 19:42:42 -0500

    Create PublishToStratusTonight.cs

---
+++ PublishToStratusTonight.cs
@@ -0,0 +1,114 @@
+/*
+ * Created by SharpDevelop.
+ * User: GTP Innovate 2024
+ * Date: 8/24/2024
+ * Time: 3:00 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;            // We had to EXPLICITLY right click on "References" to get access to this code (Timer lives here)
+using System.Runtime.InteropServices;  // We need this to keep our computer from going into sleep mode 
+
+namespace Utilities
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId("0962CE0E-1BAE-4461-8737-E4F3F4451ED5")]
+    public partial class ThisApplication
+    {			
+      private int _hourToPublish = 22; // 22 = 10pm military time
+      private Timer _scheduler;
+      private void Module_Startup(object sender, EventArgs e)
+      {		
+         _scheduler = new Timer();
+         _scheduler.Interval = 1000; // 1000 = 1 second. Eevery second, check to see if it is time to publish.       
+         _scheduler.Tick += SchedulerCallback;
+      }
+		  
+      private void Module_Shutdown(object sender, EventArgs e)
+      {			
+      }
+		
+      #region Revit Macros generated code
+      private void InternalStartup()
+      {
+         this.Startup += new System.EventHandler(Module_Startup);
+         this.Shutdown += new System.EventHandler(Module_Shutdown);
+      }
+      #endregion
+
+      [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)] // Turn on/off Windows Sleep
+      static extern uint SetThreadExecutionState(uint esFlags);            
+		
+      /// <summary>
+      /// MACRO ENTRY POINT -- Revit calls into here
+      /// </summary>
+		  public void PublishToStratusTonight()
+		  {					
+		     // If we are already running a scheduler, then ask them if they want to cancel it.
+		 	   if (_scheduler.Enabled)
+         {
+            var select = TaskDialog.Show("Already Scheduled to Publish", "Would you like to cancel the scheduled publis?", TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No);            
+	          if (select == TaskDialogResult.Yes)
+	          {
+                SetThreadExecutionState(1); // Turn off Stay Awake
+               _scheduler.Stop();
+	          }
+            return;
+         }
+
+			   // Ask them if they really want to schedule a silent publish (show them YES and NO buttons)
+         var doIt = TaskDialog.Show("Continue?", 
+                                    "Would you like to schedule a silent publish for military hour " + _hourToPublish + "?",
+                                    TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No);
+			
+	       if (doIt == TaskDialogResult.Yes)
+	       {
+            SetThreadExecutionState(0x80000001); // 80,000,001 --> Tell Windows to Stay Awake (may let monitor sleep, but that's okay);
+            _scheduler.Start();                  // Start the scheduler, which will check to see when it is the _hourToPublish (see SchedulerCallback)
+         }
+		  }
+		
+     /// -------------------------------------------------------------
+     /// This is the Timer function that gets called once ever so often
+     /// and checks to see if we have hit the desired hour of the day
+     /// to Silent Publish (set _hourToPublish to control schedule)
+     /// -------------------------------------------------------------
+     private void SchedulerCallback(object sender, EventArgs e)
+     {			
+        if (_scheduler.Enabled && DateTime.Now.Hour == _hourToPublish) // 22 = 10pm military time
+        {
+          _scheduler.Stop();				
+          RunSilentPublishNow();
+        }
+     }
+
+     /// -------------------------------------------------------------
+     /// This is the function that does the actual Silent Publish 
+     /// command. This will get called by the scheduler at the right
+     /// hour of the day
+     /// -------------------------------------------------------------
+     private void RunSilentPublishNow()
+     {
+        Document doc = this.ActiveUIDocument.Document; 
+        var app = doc.Application;
+        UIApplication uiapp = new UIApplication(app);
+       
+        try
+        {
+           //Look in C:\ProgramData\Autodesk\ApplicationPlugins\Gtpx.ModelSync.Addin.Revit2021.bundle\Contents\Gtpx.ModelSync.Addin.Revit2021.addin					
+           var addinId = Autodesk.Revit.UI.RevitCommandId.LookupCommandId("a584d14f-3ba0-4f78-8719-7640168260ec");
+           uiapp.PostCommand(addinId);
+        }
+        catch (Exception ex)
+        {
+            TaskDialog.Show("Publish to Stratus Tonight", "ERROR: Could not launch silent publish: " + ex.Message);
+        }
+     }
+   }
+}
